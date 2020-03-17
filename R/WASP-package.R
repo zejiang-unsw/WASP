454b12b6a244b28198d7e1c45ee0b0f9d2fc91f4
zejiang-unsw <47626771+zejiang-unsw@users.noreply.github.com>
2020-03-17 16:26:59 +1100

    Create WASP-package.R

---
+++ R/WASP-package.R
@@ -0,0 +1,32 @@
+#' WASP: WAvelet System Prediction
+#'
+#' The package WASP (variance transformation) is used for system modelling and prediction. 
+#' 
+#' 
+#' @details
+#' \tabular{ll}{
+#' Package: \tab WASP\cr
+#' Type: \tab Package\cr
+#' Version: \tab 1.0.1\cr
+#' Date: \tab 2020-03-17\cr
+#' License: \tab GPL-3\cr
+#' }
+#' 
+#' @author 
+#' Ze Jiang
+#' 
+#' Maintainer: Ze Jiang zejiangunsw@gamil.com
+#' 
+#' @references 
+#' Jiang, Z., Sharma, A., & Johnson, F. (2020). Refining predictor spectral representation using wavelet theory for improved natural system modelling. Water Resources Research, n/a(n/a), e2019WR026962. doi:10.1029/2019wr026962
+#'
+#' @section WASP functions:
+#' Variance transformation functions: dwt.vt, modwt.vt, at.vt and associated 
+#' 
+#' SPI calculation function
+#' 
+#' Synthetic data generator functions
+#' 
+#' @docType package
+#' @name WASP-package
+NULL
