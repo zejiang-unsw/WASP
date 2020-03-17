c0cb01fa6d883eb069d90bb5faa2645017572345
zejiang-unsw <47626771+zejiang-unsw@users.noreply.github.com>
2020-03-17 14:19:06 +1100

    remove zoo dependence

--- R/SPI_calc.R
+++ R/SPI_calc.R
@@ -3,10 +3,8 @@
 #' @param prec.zoo		  A zoo series contain date and rainfall vector/matrix.
 #' @param sc		        The accumulation period in months. Commonly 6, 12, 24, 36, and 48 months.
 #' @param method        A character string coding for the fitting method: "mle" for 'maximum likelihood estimation', "mme" for 'moment matching estimation', "qme" for 'quantile matching estimation' and "mge" for 'maximum goodness-of-fit estimation'.
-
+#' 
 #' @return A matrix of time sereis.
-#' @import fitdistrplus
-#' @import zoo
 #' @export
 #'
 #' @examples
@@ -19,7 +17,7 @@
 #' par(mfrow=c(3,5))
 #' for(i in 1:ncol(SPI)) plot(SPI[,i],SPI.12[,i])
 
-SPI.calc <- function(prec.zoo,sc=24, method="mme"){
+SPI.calc <- function(prec.zoo,sc=24, method="mle"){
 ###spi calculation
 
 ### Input
@@ -30,7 +28,9 @@ SPI.calc <- function(prec.zoo,sc=24, method="mme"){
 # Calculate running sum of different duration like 6,12 24,48 etc)
 # rollsumr(..., align = "right"), rank is applied to zoo series, align which is the date you took
 #run_sum <- apply(prec,2,rollsum,k=sc) # if prec is a rainfall matrix without date
-run_sum <- rollsumr(prec.zoo,k=sc) # if prec is a zoo objective
+#run_sum <- rollsumr(prec.zoo,k=sc) # if prec is a zoo objective
+
+run_sum <- na.omit(stats::filter(prec.zoo, rep(1, sc)))
 # number of data after rollsum, length(date) -sc + 1 = nrow(run_sum)
 
 ### Estimate shape and rate parameter of gamma distribution from data
@@ -41,7 +41,9 @@ nrow <- ifelse(is.null(ncol(run_sum)),length(run_sum),nrow(run_sum))
 for (i in 1:ncol){
   # "mle" for 'maximum likelihood estimation'
   # lower: lower limit of the parameters: shape>0 and rate>0 by definition
-  pd <-  fitdist(as.numeric(run_sum[ ,i]),"gamma", method = method, lower = c(0, 0))
+  pd <-  fitdistrplus::fitdist(as.numeric(run_sum[ ,i]),"gamma", method = method, lower = c(0, 0))
+  #pd <-  MASS::fitdistr(as.numeric(run_sum[ ,i]),"gamma")
+  
   alpha[i]= pd$estimate[1] # shape
   beta[i] = pd$estimate[2] # rate
 }
