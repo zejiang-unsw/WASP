f3b5a2fe3924cf40ea81e982489cf0842fad4d5a
zejiang-unsw <47626771+zejiang-unsw@users.noreply.github.com>
2020-04-08 14:35:36 +1000

    modwt remove vt.opt and pad

--- R/modwt_vt.R
+++ R/modwt_vt.R
@@ -3,12 +3,10 @@
 #' @param data		  A list of response x and dependent variables dp.
 #' @param wf		    Name of the wavelet filter to use in the decomposition.
 #' @param J      	  Specifies the depth of the decomposition. This must be a number less than or equal to log(length(x),2).
-#' @param pad		    The method used for extend data to dyadic size. Use "per", "zero", or "sym".
 #' @param boundary  Character string specifying the boundary condition. If boundary=="periodic" the default, then the vector you decompose is assumed to be periodic on its defined interval, if boundary=="reflection", the vector beyond its boundaries is assumed to be a symmetric reflection of itself.
-#' @param vt.opt    Options of variance transformation matrix, either "Sxx" or "Cov".
 #' @param cov.opt   Options of Covariance matrix sign. Use "pos", "neg", or "auto".
 #'
-#' @return A list of 8 elements: wf, vt.opt, boundary, pad, x (data), dp (data), dp.n (variance trasnformed dp), and S (covariance matrix).
+#' @return A list of 8 elements: wf, J, boundary, x (data), dp (data), dp.n (variance trasnformed dp), and S (covariance matrix).
 #' @import waveslim
 #' @export
 #'
@@ -19,12 +17,11 @@
 #' ###real-world example
 #' x <- window(SPI.12,start=c(1950,1),end=c(2009,12))
 #' dp <- window(obs.mon,start=c(1950,1),end=c(2009,12))
-#' vt.opt = ifelse(0,"Sxx","Cov")
 #'
 #' for(id in 5){
 #'
 #'   data <- list(x=x[,id],dp=dp)
-#'   dwt <- modwt.vt(data, wf="d4", J=7, pad="zero", boundary="periodic", vt.opt)
+#'   dwt <- modwt.vt(data, wf="d4", J=7, boundary="periodic")
 #'
 #'   par(mfrow=c(ncol(dp),1),pty="m",mar=c(1,4,1,2))
 #'   for(i in 1:ncol(dp))
@@ -35,7 +32,7 @@
 #' fd <- c(3,5,10,15,25,30,55,70,95)
 #'
 #' data.SW1 <- data.gen.SW(nobs=512,fp=25,fd=fd)
-#' dwt.SW1 <- modwt.vt(data.SW1, wf="d4", J=7, pad="zero", boundary="periodic",vt.opt)
+#' dwt.SW1 <- modwt.vt(data.SW1, wf="d4", J=7, boundary="periodic")
 #'
 #' x.modwt <- waveslim::modwt(dwt.SW1$x, wf = "d4", n.levels = 7,  boundary = "periodic")
 #' dp.modwt <- waveslim::modwt(dwt.SW1$dp[,1], wf = "d4", n.levels = 7,  boundary = "periodic")
@@ -52,10 +49,10 @@
 #' lines(x = bar[2,], y = sapply(x.modwt,var)/sum(sapply(x.modwt,var)))
 #' points(x = bar[2,], y = sapply(x.modwt,var)/sum(sapply(x.modwt,var)))
 
-modwt.vt <- function(data, wf, J, pad, boundary, vt.opt=c("Sxx","Cov"), cov.opt=c("auto","pos","neg")){
+modwt.vt <- function(data, wf, J, boundary, cov.opt=c("auto","pos","neg")){
 
   # initialization
-  x= data$x; dp= data$dp
+  x= data$x; dp= as.matrix(data$dp)
   mu.dp <- apply(dp,2,mean)
 
   # variance transfrom
@@ -63,9 +60,6 @@ modwt.vt <- function(data, wf, J, pad, boundary, vt.opt=c("Sxx","Cov"), cov.opt=
   S <- matrix(nrow=J+1,ncol=ndim)
   dp.n <- matrix(nrow=n,ncol=ndim)
   modwt.dp <- vector("list", ndim)
-
-  modwt.x <- waveslim::modwt(x, wf = wf, n.levels = J, boundary = boundary)
-  Sxx <- unlist(lapply(modwt.x, sd))
 
   for(i in 1:ndim){
     #center
@@ -83,32 +77,32 @@ modwt.vt <- function(data, wf, J, pad, boundary, vt.opt=c("Sxx","Cov"), cov.opt=
     if(dif>10^-10) warning(paste0("Difference between Reconstructed and original:",dif))
 
     # variance transformation
-    if(vt.opt!="Sxx") cov <- cov(x, Bn) else cov <- Sxx
+    cov <- cov(x, Bn)
     if(cov.opt=="pos") cov <- cov else if(cov.opt=="neg") cov <- -cov
     S[,i] <- as.vector(cov)
 
     Vr <- as.numeric(cov/norm(cov,type="2")*sd(dp.c))
-    B.n <- sapply(1:length(Vr), function(i) Bn[,i]*Vr[i])
 
-    B.n.ls <- class.modwt(B.n, J, wf, boundary)
-    dp.n[,i] <- imodwt(B.n.ls) + mu.dp[i]
+    dp.n[,i] <- Bn%*%Vr + mu.dp[i]
 
-    # modwt.x <- waveslim::modwt(imodwt(B.n.ls), wf = wf, n.levels = J, boundary = boundary)
-    # Sxx.dp.n <- unlist(lapply(modwt.x, sd))
-    # barplot(cbind(Sxx, Vr, Sxx.dp.n), beside=TRUE, ylim=c(0,3))
+    # B.n <- sapply(1:length(Vr), function(i) Bn[,i]*Vr[i])
+    # #cat(sum(apply(B.n,2,var)), "\n")
+    # B.n.ls <- class.modwt(B.n, J, wf, boundary)
+    # dp.n[,i] <- imodwt(B.n.ls) + mu.dp[i]
 
     #check the correlation after vt then decide the direction of C
     if(cov.opt=="auto"){
-      if(cor(dp.n[,i],dp[,i])<0) cov <- -cov
+      #if(cor(dp.n[,i],dp[,i])<0) cov <- -cov
       #cat(cor.test(dp.n[,i],dp[,i])$p.value,"&",cor.test(dp.n[,i],dp[,i])$estimate,"\n")
-      #if(cor.test(dp.n[,i],dp[,i])$estimate<0&cor.test(dp.n[,i],dp[,i])$p.value<0.05) cov <- -cov
+      if(cor.test(dp.n[,i],dp[,i])$estimate<0&cor.test(dp.n[,i],dp[,i])$p.value<0.05) cov <- -cov
       S[,i] <- as.vector(cov)
 
       Vr <- as.numeric(cov/norm(cov,type="2")*sd(dp.c))
-      B.n <- sapply(1:length(Vr), function(i) Bn[,i]*Vr[i])
+      dp.n[,i] <- Bn%*%Vr + mu.dp[i]
 
-      B.n.ls <- class.modwt(B.n, J, wf, boundary)
-      dp.n[,i] <- imodwt(B.n.ls) + mu.dp[i]
+      # B.n <- sapply(1:length(Vr), function(i) Bn[,i]*Vr[i])
+      # B.n.ls <- class.modwt(B.n, J, wf, boundary)
+      # dp.n[,i] <- imodwt(B.n.ls) + mu.dp[i]
 
     }
 
@@ -118,15 +112,15 @@ modwt.vt <- function(data, wf, J, pad, boundary, vt.opt=c("Sxx","Cov"), cov.opt=
     dif.var <- (var(dp.c)-sum(unlist(apply(B,2,var))))/var(dp.c)
     if(dif.var>0.15) warning(paste0("df.var.origin between Reconstructed and original(percentage):",dif.var*100))
 
-    dif.var <- (var(dp.c)-sum(unlist(apply(B.n,2,var))))/var(dp.c)
+    dif.var <- (var(dp.c)-sum(unlist(apply(Bn%*%Vr,2,var))))/var(dp.c)
     if(dif.var>0.15) warning(paste0("df.var.vt between Reconstructed and original(percentage):",dif.var*100))
 
   }
 
   dwt <- list(wavelet = wf,
-              vt.opt = vt.opt,
+              J = J,
               boundary = boundary,
-              pad = pad,
+
 
               x=x,
               dp=dp,
@@ -146,7 +140,7 @@ modwt.vt <- function(data, wf, J, pad, boundary, vt.opt=c("Sxx","Cov"), cov.opt=
 #' @param J      	  Specifies the depth of the decomposition. This must be a number less than or equal to log(length(x),2).
 #' @param dwt       A class of "dwt" data. Output from dwt.vt().
 #'
-#' @return          A list of 8 elements: wf, vt.opt, boundary, pad, x (data), dp (data), dp.n (variance trasnformed dp), and S (covariance matrix).
+#' @return          A list of 8 elements: wf, J, boundary, x (data), dp (data), dp.n (variance trasnformed dp), and S (covariance matrix).
 #' @export
 #' @references Z Jiang, A Sharma, and F Johnson. WRR
 #'
@@ -166,7 +160,7 @@ modwt.vt <- function(data, wf, J, pad, boundary, vt.opt=c("Sxx","Cov"), cov.opt=
 #' }
 #'
 #' ## variance transformation - calibration
-#' dwt.list<- lapply(data.list, function(x) modwt.vt(x, wf="d4", J=7, pad="zero", boundary="periodic"))
+#' dwt.list<- lapply(data.list, function(x) modwt.vt(x, wf="d4", J=7, boundary="periodic"))
 #'
 #' ##response SPI - validation
 #' SPI.val <- SPI.calc(window(rain.mon, start=c(1979,1), end=c(2009,12)),sc=12)
@@ -198,9 +192,9 @@ modwt.vt <- function(data, wf, J, pad, boundary, vt.opt=c("Sxx","Cov"), cov.opt=
 modwt.vt.val <- function(data, J, dwt){
 
   # initialization
-  x= data$x; dp= data$dp
+  x= data$x; dp= as.matrix(data$dp)
   mu.dp <- apply(dp,2,mean)
-  wf <- dwt$wavelet; boundary <- dwt$boundary; vt.opt <- dwt$vt.opt; pad <- dwt$pad
+  wf <- dwt$wavelet; boundary <- dwt$boundary
 
   # variance transfrom
   ndim=ncol(dp);n=nrow(dp);
@@ -228,25 +222,28 @@ modwt.vt.val <- function(data, J, dwt){
     } else { cov[1:length(dwt$S[,i])] <- dwt$S[,i] }
 
     Vr <- as.numeric(cov/norm(cov,type="2")*sd(dp.c))
-    B.n <- sapply(1:length(Vr), function(i) Bn[,i]*Vr[i])
 
-    B.n.ls <- class.modwt(B.n, J, wf, boundary)
-    dp.n[,i] <- imodwt(B.n.ls) + mu.dp[i]
+    dp.n[,i] <- Bn%*%Vr  + mu.dp[i]
+
+    # B.n <- sapply(1:length(Vr), function(i) Bn[,i]*Vr[i])
+    # B.n.ls <- class.modwt(B.n, J, wf, boundary)
+    # dp.n[,i] <- imodwt(B.n.ls) + mu.dp[i]
+
 
     #statistic check
     dif.var <- (var(dp.c)-sum(unlist(apply(B,2,var))))/var(dp.c)
     if(dif.var>0.15) warning(paste0("df.var.origin between Reconstructed and original(percentage):",dif.var*100))
 
-    dif.var <- (var(dp.c)-sum(unlist(apply(B.n,2,var))))/var(dp.c)
+    dif.var <- (var(dp.c)-sum(unlist(apply(Bn%*%Vr,2,var))))/var(dp.c)
     if(dif.var>0.15) warning(paste0("df.var.vt between Reconstructed and original(percentage):",dif.var*100))
 
 
   }
 
   dwt <- list(wavelet = wf,
-              vt.opt = vt.opt,
+              J = J,
               boundary = boundary,
-              pad = pad,
+
 
               x=x,
               dp=dp,
@@ -269,17 +266,16 @@ modwt.vt.val <- function(data, J, dwt){
 #' @param boundary  Character string specifying the boundary condition. If boundary=="periodic" the default, then the vector you decompose is assumed to be periodic on its defined interval, if boundary=="reflection", the vector beyond its boundaries is assumed to be a symmetric reflection of itself.
 #'
 #' @return          A class of "modwt" object
-#' @export
-#'
+# #' @export
 
-class.modwt <- function(B.modwt, J, wf, boundary){
-
-  B.modwt.ls <- as.list(as.data.frame(B.modwt))
-  names(B.modwt.ls) <- c(paste0("d", 1:J),paste0("s",J))
-
-  attributes(B.modwt.ls)$class <- "modwt"
-  attributes(B.modwt.ls)$wavelet <- wf
-  attributes(B.modwt.ls)$boundary <- boundary
-
-  return(B.modwt.ls)
-}
+# class.modwt <- function(B.modwt, J, wf, boundary){
+#
+#   B.modwt.ls <- as.list(as.data.frame(B.modwt))
+#   names(B.modwt.ls) <- c(paste0("d", 1:J),paste0("s",J))
+#
+#   attributes(B.modwt.ls)$class <- "modwt"
+#   attributes(B.modwt.ls)$wavelet <- wf
+#   attributes(B.modwt.ls)$boundary <- boundary
+#
+#   return(B.modwt.ls)
+# }
